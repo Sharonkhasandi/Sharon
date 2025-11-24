6529b4d306a8ddc4f98c76575f8b74597750d64b
Sharonkhasandi <khasandisharoh@gmail.com>
2025-11-24 06:10:34 +0300

    Fix Express 5 wildcard route

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/api/contact', async (req, res) => {
 });
 
 // Fallback to index.html for client-side routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
